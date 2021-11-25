cca7d3a9f806756be3f01cc7cb87de7bd0bb1820
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-11-26 00:11:18 +0530

    Sample email ids test

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static boolean passwordValidation(String password) {
-        String pattern ="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*d)(?=.*[@#$%^&-+=()]).{8,}$";
-        Pattern pass= Pattern.compile(pattern);
-        Matcher match = pass.matcher(password);
+    public static boolean passwordValidation(String email) {
+        String pattern ="([a-zA-Z0-9.-_]{3,})[a-zA-Z0-9.-_]*[@][a-zA-Z0-9.-_]*[a-zA-Z0-9.-_]{3,}[a-zA-Z0-9.-_]*";
+        Pattern pass = Pattern.compile(pattern);
+        Matcher match = pass.matcher(email);
         if (match.matches()) {
             return true;
         }
@@ -16,6 +16,6 @@ public class UserRegistration {
     }
 
     public static void main(String[] args) {
-        System.out.println(passwordValidation("aaaZ44@"));
+        System.out.println(passwordValidation("amolnagose12@gmail.com"));
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -7,10 +7,49 @@ import static org.junit.Assert.*;
 
 public class UserRegistrationTest {
     @Test
-    public void checkValidPassword() {
+    public void checkValidEmail() {
         UserRegistration user = new UserRegistration();
-        boolean pass = user.passwordValidation("aaaS44@");
+        boolean pass = user.passwordValidation("abc.zxy@yahoo.com.in");
         Assert.assertSame(true,pass);
     }
+    @Test
+    public void checkValidEmail2(){
+        UserRegistration user = new UserRegistration();
+        boolean pass = user.passwordValidation("abc-100@yahoo.com");
+        Assert.assertSame(true,pass);
+    }
+    @Test
+    public void checkValidEmail3(){
+        UserRegistration user = new UserRegistration();
+        boolean pass = user.passwordValidation("abc.100@yahoo.com");
+        Assert.assertSame(true,pass);
+    }
+    @Test
+    public void checkValidEmail4(){
+        UserRegistration user = new UserRegistration();
+        boolean pass = user.passwordValidation("abc111@abc.com");
+        Assert.assertSame(true,pass);
+    }
+    @Test
+    public void checkValidEmail5(){
+        UserRegistration user = new UserRegistration();
+        boolean pass = user.passwordValidation("abc-100@yahoo.net");
+        Assert.assertSame(true,pass);
+    }
+    @Test
+    public void checkValidEmail6(){
+        UserRegistration user = new UserRegistration();
+        boolean pass = user.passwordValidation("abc.100@abc.com.au");
+        Assert.assertSame(true,pass);
+    }
+    @Test
+    public void checkValidEmail7(){
+        UserRegistration user = new UserRegistration();
+        boolean pass = user.passwordValidation("abc@gmail.com.com");
+        Assert.assertSame(true,pass);
+    }
+
+
+
 
 }
